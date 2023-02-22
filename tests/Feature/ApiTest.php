cab74c28e03fe3ffef27936a2bee1068717c8c28
Geo12862 <74008077+Geo12862@users.noreply.github.com>
2023-02-22 23:28:03 +0100

    Test del 5 al 11
    Abril

--- tests/Feature/ApiTest.php
+++ tests/Feature/ApiTest.php
@@ -36,4 +36,113 @@ class ApiTest extends TestCase
         $response->assertStatus(200);
         $response->assertJsonCount(3, 'data');
     }
+    
+    public function test_can_fetch_single_communities()
+    {
+        $user = User::factory()->create();
+        $this->actingAs($user);
+
+        $community = Community::factory()->create();
+        $response = $this->getJson("api/communities/{$community->id}");
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'name' => $community->name,
+                'description' => $community->description
+            ]
+        ]);
+
+        $response = $this->get('/api/communities/1');
+
+        $response->assertStatus(200);
+        
+    }
+
+    public function text_name_of_communities_is_required()
+    {
+        Sanctum::actingAs(factory(User::class)->create());
+        $response = $this->postJson('/api/communities', [
+            'name' => ''
+        ]);
+
+        $response->assertStatus(422);
+        $response->assertJson([
+            'message' => 'The given data was invalid.',
+            'errors' => [
+                'name' => [
+                    'The name field is required.'
+                ]
+            ]
+        ]);
+
+
+    }
+
+    public function test_can_create_communities()
+    {
+        Sanctum::actingAs(factory()->create());
+        $response = $this->postJson('/api/communities', [
+            'name' => 'Test Communities'
+        ]);
+
+        $response->assertStatus(201);
+        $response->assertJson([
+            'data' => [
+                'name' => 'Test Communities'
+            ]
+        ]);
+    }
+
+    public function test_guest_cannot_create_communities()
+    {
+        $response = $this->postJson('/api/communities', [
+            'name' => 'Test Communities'
+        ]);
+
+        $response->assertStatus(401);
+    }
+
+    public function test_can_update_communities()
+    {
+        Sanctum::actingAs(factory()->create());
+        $response = $this->putJson('/api/communities/1', [
+            'name' => 'Test Communities'
+        ]);
+
+        $response->assertStatus(200);
+        $response->assertJson([
+            'data' => [
+                'name' => 'Test Communities'
+            ]
+        ]);
+    }
+
+    public function test_can_delete_communities()
+    {
+        Sanctum::actingAs(factory()->create());
+        $response = $this->deleteJson('/api/communities/1');
+
+        $response->assertStatus(204);
+    }
+
+    public function test_can_returns_a_json_api_error_object_when_a_community_is_not_found()
+    {
+        $user = factory()->create();
+
+        $response = $this 
+            ->actingAs($user)
+            ->from('/api/communities/1')
+            ->deleteJson('/api/communities/1', [
+                'password' => 'wrong-password',
+            ]);
+
+        $response
+            ->assertStatus(404)
+            ->assertRedirect('/api/communities/1');
+
+
+        $this->assertNotNull($user->fresh());
+
+    }
 }
